e8f734b2326e94883c8814aa250f13ca5f66c3da
Soorajskr <157726559+Soorajskr@users.noreply.github.com>
2024-09-19 12:39:11 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rgname" {
+    name = var.rgname
+    location = var.location
+  
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name = var.vnetname
+    location = var.location
+    resource_group_name = var.rgname
+    address_space = [var.vnetcidr]
+  
+}
---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "rgname" {
+    type = string
+  
+}
+
+variable "location" {
+    type = string
+  
+}
+
+variable "vnetname" {
+    type = string
+  
+}
+
+variable "vnetcidr" {
+    type =string
+  
+}
